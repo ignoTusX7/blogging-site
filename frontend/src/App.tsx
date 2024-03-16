4f55b41f136bd77d6280e2ef7b7a94c874f07bc2
moksh <moxvankar2005@gmail.com>
2024-03-16 10:04:39 +0530

    added routes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,22 @@
-
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Signup } from "./pages/Signup";
+import { Signin } from "./pages/Signin";
+import { Blog } from "./pages/Blog";
+import { Home } from "./pages/Home";
 
 function App() {
- 
   return (
     <>
-    <p className='bg-gray-500'>Hello</p>
-      
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/signin" element={<Signin />} />
+          <Route path="/blog/:slug" element={<Blog />} />
+        </Routes>
+      </BrowserRouter>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
