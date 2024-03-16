67471f8f757f9e1abc8bf7d775235d5c3710a8d9
moksh <moxvankar2005@gmail.com>
2024-03-16 12:23:01 +0530

    fixed cors issue

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,14 +1,16 @@
 import { Context, Hono } from "hono";
 import mainRouter from "./routes";
 
+import { cors } from "hono/cors";
 
 const app = new Hono<{
 	Bindings: {
-		DATABASE_URL: string,
-		JWT_SECRET: string,
-	}
+		DATABASE_URL: string;
+		JWT_SECRET: string;
+	};
 }>();
 
+app.use("/*", cors());
 
 app.route("/api/v1", mainRouter);
 
